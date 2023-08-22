0307e68899dab2eb5b9b4071fa59445c061ed05f
tojaarek <119487863+tojaarek@users.noreply.github.com>
2023-08-22 15:25:30 +0200

    mobile menu bugfix

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -12,7 +12,9 @@ icon.addEventListener('click', () => {
 const mobileMenu = document.querySelector('.js-menu-container');
 const openMenuButton = document.querySelector('.js-open-menu');
 const header = document.querySelector('.header');
-const mobileNavLink = document.querySelectorAll('.mobile-nav__link');
+const mobileNavLink1 = document.getElementById('aboutLink');
+const mobileNavLink2 = document.getElementById('realLink');
+const mobileNavLink3 = document.getElementById('contactLink');
 
 const toggleMenu = () => {
   const isMenuOpen = mobileMenu.classList.toggle('is-open');
@@ -20,7 +22,25 @@ const toggleMenu = () => {
 };
 
 openMenuButton.addEventListener('click', toggleMenu);
-mobileNavLink.addEventListener('click', toggleMenu);
+mobileNavLink1.addEventListener('click', () => {
+  mobileMenu.classList.remove('is-open');
+  icon1.classList.toggle('a');
+  icon2.classList.toggle('c');
+  icon3.classList.toggle('b');
+});
+mobileNavLink2.addEventListener('click', () => {
+  mobileMenu.classList.remove('is-open');
+  icon1.classList.toggle('a');
+  icon2.classList.toggle('c');
+  icon3.classList.toggle('b');
+});
+
+mobileNavLink3.addEventListener('click', () => {
+  mobileMenu.classList.remove('is-open');
+  icon1.classList.toggle('a');
+  icon2.classList.toggle('c');
+  icon3.classList.toggle('b');
+});
 
 window.matchMedia('(min-width: 1024px)').addEventListener('change', event => {
   if (!event.matches) return;
