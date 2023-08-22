d2ecaaae87d866db6e29914e5f5e2d010203f1c0
tojaarek <119487863+tojaarek@users.noreply.github.com>
2023-08-22 09:09:45 +0000

    Deploying to gh-pages from @ tojaarek/adrespect@50a6ced5ae0b63446e2fbfd1296d97577346d474 🚀

---
+++ index.8a711fcb.js
@@ -0,0 +1,2 @@
+window.addEventListener("scroll",(function(){const e=document.querySelectorAll(".cards__item"),t=document.querySelector(".gallery"),n=t.getBoundingClientRect().top;e.forEach((e=>{e.getBoundingClientRect().top<=1100&&e.classList.add("animate__fadeInUp")})),n<=1100&&t.classList.add("animate__fadeIn")}));
+//# sourceMappingURL=index.8a711fcb.js.map
