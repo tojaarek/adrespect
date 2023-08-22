f0eb76fd544bdb12547101f4a68520c10606fe26
tojaarek <119487863+tojaarek@users.noreply.github.com>
2023-08-22 15:08:24 +0200

    prettier

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -1,31 +1,31 @@
-const icon = document.getElementById("icon");
-const icon1 = document.getElementById("a");
-const icon2 = document.getElementById("b");
-const icon3 = document.getElementById("c");
+const icon = document.getElementById('icon');
+const icon1 = document.getElementById('a');
+const icon2 = document.getElementById('b');
+const icon3 = document.getElementById('c');
 
-icon.addEventListener("click", () => {
-  icon1.classList.toggle("a");
-  icon2.classList.toggle("c");
-  icon3.classList.toggle("b");
+icon.addEventListener('click', () => {
+  icon1.classList.toggle('a');
+  icon2.classList.toggle('c');
+  icon3.classList.toggle('b');
 });
 
-const mobileMenu = document.querySelector(".js-menu-container");
-const openMenuButton = document.querySelector(".js-open-menu");
-const header = document.querySelector(".header");
-const mobileNavLink = document.querySelectorAll(".mobile-nav__link");
+const mobileMenu = document.querySelector('.js-menu-container');
+const openMenuButton = document.querySelector('.js-open-menu');
+const header = document.querySelector('.header');
+const mobileNavLink = document.querySelectorAll('.mobile-nav__link');
 
 const toggleMenu = () => {
-  const isMenuOpen = mobileMenu.classList.toggle("is-open");
-  header.classList.toggle("header--border");
+  const isMenuOpen = mobileMenu.classList.toggle('is-open');
+  header.classList.toggle('header--border');
 };
 
-openMenuButton.addEventListener("click", toggleMenu);
-mobileNavLink.addEventListener("click", toggleMenu)
+openMenuButton.addEventListener('click', toggleMenu);
+mobileNavLink.addEventListener('click', toggleMenu);
 
-window.matchMedia("(min-width: 1024px)").addEventListener("change", (event) => {
+window.matchMedia('(min-width: 1024px)').addEventListener('change', event => {
   if (!event.matches) return;
-  mobileMenu.classList.remove("is-open");
-  icon1.classList.toggle("a");
-  icon2.classList.toggle("c");
-  icon3.classList.toggle("b");
+  mobileMenu.classList.remove('is-open');
+  icon1.classList.toggle('a');
+  icon2.classList.toggle('c');
+  icon3.classList.toggle('b');
 });
